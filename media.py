03944e2ce51dae070658d55693ec8a1b8b0f61c4
Thalente Ngcobo <thalente0ngcobo@gmail.com>
2022-03-08 01:09:11 +0200

    Added context menu -- Right click action

--- media.py
+++ media.py
@@ -3,7 +3,7 @@ import os
 from player import Player
 from view.ui import Ui_MainWindow
 
-from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5.QtWidgets import QMainWindow, QApplication, QAction, QMenu
 from PyQt5.QtGui import QIcon
 
 
@@ -54,3 +54,57 @@ class MediaPlayer(QMainWindow, Ui_MainWindow):
 
         # Hide playlist widget
         self.mainFrame.hide()
+
+    def contextMenuEvent(self, event):
+        """
+        Context menu
+        """
+        context_menu = QMenu(self)
+
+        if self.player.is_playing():
+            button_text = "Pause"
+        else:
+            button_text = "Play"
+
+        play_button = context_menu.addAction(button_text)
+        stop_button = context_menu.addAction("Stop")
+        next_button = context_menu.addAction("Next")
+        previous_button = context_menu.addAction("Previous")
+
+        context_menu.addSeparator()
+        playlist_button = context_menu.addAction("Playlist")
+
+        impMenu = QMenu("Open Media", self)
+        open_file = QAction("Open File..", self)
+        open_dir = QAction("Open Directory..", self)
+        impMenu.addAction(open_file)
+        impMenu.addAction(open_dir)
+        sub_menu = context_menu.addMenu(impMenu)
+
+        context_menu.addSeparator()
+        quitAct = context_menu.addAction("Quit")
+
+        action = context_menu.exec_(self.mapToGlobal(event.pos()))
+        if action == quitAct:
+            self.close()
+        elif action == play_button:
+            self.player.pause()
+        elif action == stop_button:
+            self.player.stop()
+            self.reset_ui()
+        elif action == open_file:
+            self.open_files()
+        elif action == open_dir:
+            self.open_folder()
+        elif action == next_button:
+            self.player.next()
+            print(self.player.get_title())
+        elif action == previous_button:
+            self.player.previous_media()
+            print(self.player.get_title())
+        elif action == playlist_button:
+            # self.videoFrame.hide()
+            self.player.pause()
+            self.frame_2.show()
+            self.treeWidget.show()
+            self.update()
